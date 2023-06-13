b51e8a4e318bddc7d2dfdf6622e732f7077d688d
uriel <urielg@cryptouno.app>
2023-06-13 11:37:48 -0300

    Add Crashlitics and custom font

--- app/src/main/java/com/example/elaniin_test/ui/theme/Type.kt
+++ app/src/main/java/com/example/elaniin_test/ui/theme/Type.kt
@@ -2,14 +2,27 @@ package com.example.elaniin_test.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.elaniin_test.R
+
+val Montserrat = FontFamily(
+    Font(R.font.montserrat_thin, FontWeight.Thin),
+    Font(R.font.montserrat_light, FontWeight.Light),
+    Font(R.font.montserrat_regular, FontWeight.Normal),
+    Font(R.font.montserrat_medium, FontWeight.Medium),
+    Font(R.font.montserrat_bold, FontWeight.Bold),
+    Font(R.font.montserrat_black, FontWeight.Black),
+    Font(R.font.montserrat_italic, FontWeight.Normal, FontStyle.Italic)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Montserrat,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
